73b1e5e33829285685206285a32d64c63e637dba
iTenerai <itenerai@gmail.com>
2025-01-13 20:07:47 +0100

    manifest v3 is done!

--- background.js
+++ background.js
@@ -1,83 +1,77 @@
 const prefix = "[Nightlight Repost Debug]: ";
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log('Message received:', message);
+    console.log("Message received:", message);
+
     if (message.type === "sendRequest") {
+        console.log(prefix + "Sending repost data to Nightlight");
 
-        const xhttp = new XMLHttpRequest();
-        xhttp.onloadstart = function () {
-            console.log(prefix + "Sending repost data to Nightlight");
-        };
-        xhttp.onload = function () {
+        fetch(message.requestURL)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                return response.json();
+            })
+            .then((data) => {
+                if (data.status === 0) {
+                    console.error(prefix + "Repost failed:", data.message);
+                    sendResponse({ success: false, message: data.message });
 
-            var data;
-            try {
-                data = JSON.parse(this.responseText);
-            } catch (error) {
-                console.log("Error parsing JSON response:", error);
+                    chrome.notifications.create({
+                        type: "basic",
+                        iconUrl: chrome.runtime.getURL("assets/nightycry.png"),
+                        title: "Failed to repost to Nightlight",
+                        message: `Please check if you are logged in to Nightlight, try again later or contact support with this error: ${data.message}`,
+                    });
+                } else if (data.status === 1) {
+                    console.log(prefix + "Repost successful");
+                    sendResponse({ success: true, message: "Repost successful" });
+
+                    chrome.notifications.create({
+                        type: "basic",
+                        iconUrl: chrome.runtime.getURL("assets/logo_monochrome.png"),
+                        title: "Reposted to Nightlight",
+                        message: "Tweet has been reposted to Nightlight successfully, you can view it in your Nightlight account.",
+                    });
+                }
+            })
+            .catch((error) => {
+                console.error(prefix + "Error occurred while sending the request:", error);
+                sendResponse({ success: false, message: "Request failed" });
+
                 chrome.notifications.create({
                     type: "basic",
-                    iconUrl: browser.runtime.getURL('assets/nightycry.png'),
+                    iconUrl: chrome.runtime.getURL("assets/nightycry.png"),
                     title: "Failed to repost to Nightlight",
-                    message: "Please check if you are logged in to Nightlight, try again later or contact support with this error: " + data['message'],
+                    message: "An unexpected error occurred while processing your request.",
                 });
-                return true;
-            }
+            });
 
-            if (data['status'] == 0) {
-                console.log(prefix + "Repost failed: ", data['message']);            
-                sendResponse({ success: false, message: data['message'] });
-                chrome.notifications.create({
-                    type: "basic",
-                    iconUrl: browser.runtime.getURL('assets/nightycry.png'),
-                    title: "Failed to repost to Nightlight",
-                    message: "Please check if you are logged in to Nightlight, try again later or contact support with this error: " + data['message'],
-                });
-                return true;
-            }
-            else if (data['status'] == 1) {
-                console.log(prefix + "Repost successful");
-                alert("Repost successful");
-                sendResponse({ success: true, message: "Repost successful" });
-                chrome.notifications.create({
-                    type: "basic",
-                    iconUrl: browser.runtime.getURL('assets/logo_monochrome.png'),
-                    title: "Reposted to Nightlight",
-                    message: "Tweet has been reposted to Nightlight succcessfully, you can view it in your Nightlight account",
-                });
-                return true;
-            }
-        };
-
-        xhttp.onerror = function () {
-            console.error("Error occurred while sending the request");
-            sendResponse({ success: false, message: "Request failed" });
-            return true;
-        };
-
-        xhttp.open("GET", message.requestURL);
-        xhttp.send();
+        // Return true to indicate asynchronous response
         return true;
     }
-    if(message.type === "cookieRequest") {
-        getCookies(message.name, function(cookie) {
+
+    if (message.type === "cookieRequest") {
+        getCookies(message.name, (cookie) => {
             sendResponse(cookie);
         });
+
+        // Return true to indicate asynchronous response
         return true;
     }
 });
 
 function getCookies(name, callback) {
-    chrome.cookies.get({
-        "url": "https://night-light.cz",
-        "name": name
-    }, function (cookie) {
-        if (callback) {
-            if (cookie === null) {
-                callback(null)
-            } else {
-                callback(cookie.value);
+    chrome.cookies.get(
+        {
+            url: "https://night-light.cz",
+            name: name,
+        },
+        (cookie) => {
+            if (callback) {
+                callback(cookie ? cookie.value : null);
             }
         }
-    });
+    );
 }
--- content-script.js
+++ content-script.js
@@ -45,7 +45,7 @@ function squeezeNightlightIn() {
 
         // I am NOT making the elements manually, innerHTML is all you get
         icon.innerHTML = `<div role="menuitem" tabindex="2" class="css-175oi2r r-1loqt21 r-18u37iz r-1mmae3n r-3pj75a r-13qz1uu r-o7ynqc r-6416eg r-1ny4l3l" data-testid="nightlightConfirm"><div class="css-175oi2r r-1777fci r-faml9v">
-      <img src="` + browser.runtime.getURL('assets/logo_monochrome.png') + `" alt="Nightlight Repost" width="24" height="24" class="class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1nao33i r-1q142lx"">
+      <img src="` + chrome.runtime.getURL('assets/logo_monochrome.png') + `" alt="Nightlight Repost" width="24" height="24" class="class="r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-lrvibr r-m6rgpd r-1nao33i r-1q142lx"">
       </div><div class="css-175oi2r r-16y2uox r-1wbh5a2"><div dir="ltr" class="css-146c3p1 r-bcqeeo r-1ttztb7 r-qvutc0 r-37j5jr r-a023e6 r-rjixqe r-b88u0q" style="color: rgb(231, 233, 234);"><span class="css-1jxf684 r-bcqeeo r-1ttztb7 r-qvutc0 r-poiln3">Nightlight</span></div></div></div>`
 
         icon.addEventListener('click', () => {
